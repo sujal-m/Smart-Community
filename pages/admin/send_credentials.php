7413efd7d72225c19767bbb965accfde2a2bb205
Ravitej Mulay <159104787+ravitej2005@users.noreply.github.com>
2025-12-17 03:40:07 +0530

    Remove hardcoded email credentials
    Removed hardcoded email credentials for security.

--- pages/admin/send_credentials.php
+++ pages/admin/send_credentials.php
@@ -18,8 +18,8 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'smart.community27@gmail.com';
-    $mail->Password = 'yemywajjjcwtxbky';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
